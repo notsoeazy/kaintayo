c410bc57e4c214c9097fdd62336ad10ec4a78670
Matt Rayner P. Santiago <mrsantiago@gbox.adnu.edu.ph>
2026-05-20 12:24:22 +0800

    fix: update image picker mediaTypes to use array format for compatibility

--- hooks/profile_screen_hook.ts
+++ hooks/profile_screen_hook.ts
@@ -60,7 +60,7 @@ export function useProfileScreen() {
     }
 
     const result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      mediaTypes: ['images'],
       allowsEditing: true,
       aspect: [1, 1],
       quality: 0.7,
